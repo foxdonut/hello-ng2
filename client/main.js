80a6813129b6cfef5651da32fd84d6badef0f6dc
foxdonut <fdaoud@fastmail.com>
2015-05-30 18:27:56 +0000

    the angular 2 docs do not mention appInjector, which is needed for DI to work. MOO.

--- client/main.js
+++ client/main.js
@@ -12,17 +12,47 @@ var BookEvents = require("./books/events");
 var bookResource = require("./resource/resource-jquery")("/books");
 require("./books/store")(pubsub, bookResource);
 
-var MyBookList = require("./books/bookList/component")(Pubsub);
+//var MyBookList = require("./books/bookList/component")(Pubsub);
+
+////
+
+  var BookList = function(pubsub) {
+    console.log("creating BookList");
+    var self = this;
+
+    self.onDataChange = function(bookList) {
+      self.bookList = bookList;
+    };
+    pubsub.subscribe(BookEvents.DATA, self.onDataChange);
+  };
+  BookList.annotations = [
+    new angular.ComponentAnnotation({
+      selector: "bookList",
+      injectables: [Pubsub]
+    }),
+    new angular.ViewAnnotation({
+      template: "BookList here" /* require("./bookList.html") ,
+      directives: [ BookItem, angular.For ] */
+    })
+  ];
+  BookList.parameters = [[Pubsub]];
+
+console.log("Pubsub:", Pubsub);
+
+////
 
 var App = function() {
   console.log("create App");
 };
 
 App.annotations = [
-  new angular.ComponentAnnotation({ selector: "app" }),
+  new angular.ComponentAnnotation({
+    selector: "app",
+    appInjector: [Pubsub]
+  }),
   new angular.ViewAnnotation({
     template: require("./template.html"),
-    directives: [ MyBookList ]
+    directives: [ BookList ]
   })
 ];
 
